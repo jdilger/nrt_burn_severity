39f62972e225670165027b37820d347e08c3ed79
kwoodward-sig <kwoodward@sig-gis.com>
2021-11-08 15:01:48 -0500

    new function to get date constraints from feature

--- scripts/fire_info.py
+++ scripts/fire_info.py
@@ -1,3 +1,4 @@
+import ee
 # structure:
 # { "fireName":{
 #     "start":str date
@@ -8,6 +9,16 @@
 #    }
 
 fire_dict = {
+    "hennessey": {
+        "region": [r"projects/pyregence-ee/assets/conus/nifc/hennessey_fire_2020"],
+        #"region_date": ["2021-07-29"], # seems to be unused
+        "start": ["2021-07-17"], # using discovery date or initial response date, 1 month before, a calendar year later
+        "end": ['2021-09-17'], # 1 month after, a calendar year later
+        "pre_start": '2019-07-17', # +1 month before start, one calendar yr back
+        "pre_end": '2019-09-17', # -1 month after start, one calendar year back
+    
+    
+    
     "tamarack": {
         "region": [r"projects/sig-misc-ee/assets/fire_response_2021/perimeters/20210729_Tamarack_KMZ_perimeter_only"],
         "region_date": ["2021-07-29"],
@@ -33,7 +44,7 @@ fire_dict = {
         "pre_end": '2020-09-01',
     },
 
-}
+}}
 
 
 def get_fire_by_name(fire_name: str, current_date: str = None) -> tuple:
@@ -49,3 +60,16 @@ def get_fire_by_name(fire_name: str, current_date: str = None) -> tuple:
     pre_start = fire_dict[fire_name]["pre_start"]
 
     return (region, fire_start, fire_end, pre_start, pre_end)
+
+
+# won't need fire_name bc it's going to map through one feature at a time
+def get_fire_info_from_feature(feat: ee.Feature) -> tuple:
+    
+    region = ee.Feature(feat).geometry()
+
+    post_start = ee.Feature(feat).getString('start')
+    post_end = ee.Feature(feat).getString('end')
+    pre_start = ee.Feature(feat).getString('pre_start')
+    pre_end = ee.Feature(feat).getString('pre_end')
+
+    return (region, post_start, post_end, pre_start, pre_end)
