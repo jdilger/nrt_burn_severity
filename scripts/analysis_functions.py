54cf83b01996893be91e7fa6f14937b11b2b58a7
kyle-woodward <kw.geospatial@gmail.com>
2023-04-10 14:33:59 -0400

    cleaning up from last dev effort

--- scripts/analysis_functions.py
+++ scripts/analysis_functions.py
@@ -1,5 +1,9 @@
 import ee
 import json
+import scripts.fire_info as fi
+import scripts.get_image_collections2 as gic2
+import scripts.get_image_collections as gic
+
 
 def nbr(image:ee.Image)-> ee.Image:
     return image.normalizedDifference(['NIR','SWIR2']).multiply(1000)
@@ -36,3 +40,27 @@ def miller_thresholds(rdnbr :ee.Image)-> ee.Image:
             .where(rdnbr.gt(640),3).rename('MillersThresholds')
     
     return serv
+
+def bs_calc_new(feat: ee.Feature):
+    fire = ee.Feature(feat)
+    # fire_geom = fire.geometry()
+    
+    fire = ee.Feature(fi.set_windows(fire))
+    region = fire.geometry()
+    # mode = fire.get('mode')
+    pre_start = fire.get('pre_start')
+    pre_end = fire.get('pre_end')
+    post_start = fire.get('post_start')
+    post_end = fire.get('post_end')
+    
+    sensor = "landsat"
+    pre_collection = gic2.getLandsatToa(pre_start,pre_end,region)
+    pre_img = gic.get_composite(pre_collection,gic.make_pre_composite,pre_start,pre_end)
+
+    post_collection = gic2.getLandsatToa(post_start,post_end,region)
+    post_img = gic.get_composite(post_collection,gic.make_nrt_composite, sensor) 
+    
+    rdnbr_calc = rdnbr(pre_img,post_img)
+    miller = miller_thresholds(rdnbr_calc)
+    
+    return ee.Image(miller).clip(region).select('MillersThresholds').toByte()
